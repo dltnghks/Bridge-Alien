035f8362cd9c4bbdeb4a6e5c77ffa10cc4d72a9c
dltnghks <dltrnghks@naver.com>
2024-10-20 14:03:48 +0900

    Chore : Add UI Class

--- Assets/03.Scripts/UI/UIScene.cs
+++ Assets/03.Scripts/UI/UIScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIScene : MonoBehaviour
+public class UIScene : UIBase
 {
     // Start is called before the first frame update
     void Start()
--- Assets/03.Scripts/UI/UISubItem.cs
+++ Assets/03.Scripts/UI/UISubItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UISubItem : MonoBehaviour
+public class UISubItem : UIBase
 {
     // Start is called before the first frame update
     void Start()
