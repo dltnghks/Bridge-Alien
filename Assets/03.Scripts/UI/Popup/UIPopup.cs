3d3eedcc3fee9b4ba2fa990fc11cf5b64665c1c5
Lee Soo Hwan <dltrnghks@naver.com>
2024-10-26 13:31:25 +0900

    Chore : Create UIPopup class and UISubItem class

---
+++ Assets/03.Scripts/UI/Popup/UIPopup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPopup : UIBase
+{
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        return true;
+    }
+
+    public virtual void ClosePopupUI()
+    {
+        Managers.UI.ClosePopupUI(this);
+    }
+}
