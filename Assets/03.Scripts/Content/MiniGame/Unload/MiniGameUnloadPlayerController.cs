7427e527a5583f1ede1849f1ce61e05419e64890
dltrnghks0507 <dltrnghks0507@gmail.com>
2024-11-25 02:33:10 +0900

    Fix : 플레이어 기준 왼쪽, 오른쪽 박스 들기, 놓기 수정
    - 바라보고 있는 방향 박스와 상호작용
    - 박스 하나 들고 있을 때, 감지되는 거 수정해야 됨.
    - 위쪽부터 떨어지는거 뭔가 이상한 느낌

--- Assets/03.Scripts/Content/MiniGame/Unload/MiniGameUnloadPlayerController.cs
+++ Assets/03.Scripts/Content/MiniGame/Unload/MiniGameUnloadPlayerController.cs
@@ -5,47 +5,49 @@ using UnityEngine;
 
 public class MiniGameUnloadPlayerController : IPlayerController
 {
-    public Player Player { get; set; }
-    
-    private Vector3 _holdPosition; // 기준이 되는 들고 있는 위치
     private List<MiniGameUnloadBox> _heldBoxes = new List<MiniGameUnloadBox>(); // 들고 있는 상자 스택
     private int _heldBoxesTop = 0;
     private float _boxHeight = 1f; // 상자 하나의 높이 (조정 가능)
-
+ 
+    public Player Player { get; set; }
+    public bool IsDropBox { get; set; }
+    
     public void Init(Player player)
     {
         Player = player;
-        _holdPosition = Player.transform.localPosition;
         _heldBoxesTop = 0;
         _heldBoxes.Clear();
+        IsDropBox = false;
     }
     
     public void InputJoyStick(Vector2 input)
     {
-        if (input.x > 0)
-        {
-            _holdPosition = Player.transform.position + new Vector3(1f, 0f, 0f);
-        }
-        else if (input.x < 0)
-        {
-            _holdPosition = Player.transform.position + new Vector3(1f, 0f, 0f);
-        }
-        Debug.Log("_holdPosition : " + _holdPosition);
         // 플레이어 이동
         Player.PlayerMovement(input);
     }
 
     public void Interaction()
     {
-        // 상자 들기
-        PickupBox();
+        if (!IsDropBox)
+        {
+            // 상자 들기
+            PickupBox();   
+        }
+        else
+        {
+            DropBox();
+        }
     }
 
     private void PickupBox()
     {
+        Debug.Log("Pickup box");
         // 플레이어 앞에 상자가 있는지 확인
         Vector3 rayOrigin = Player.transform.position; // 레이 시작 위치
-        Vector3 rayDirection = Player.CharacterDirection; // 레이 방향
+        Vector3 rayDirection;
+        if (Player.IsRight) rayDirection = Vector3.right;
+        else rayDirection = Vector3.left;
+            
         float rayDistance = 2f; // 감지 거리
 
         // 레이를 시각적으로 표시
@@ -69,20 +71,20 @@ public class MiniGameUnloadPlayerController : IPlayerController
                 var boxCollision  = box.GetComponent<BoxCollider>();
                 if (boxCollision != null)
                 {
-                    boxCollision.isTrigger = true; // 물리 효과 제거
+                    boxCollision.isTrigger = true;
                 }
 
                 
                 // 상자를 스택에 추가하고 위치 설정
                 _heldBoxes.Add(box.GetComponent<MiniGameUnloadBox>());
-
                 
                 box.transform.SetParent(Player.CharacterTransform);
-                // 위치 계산
-                box.transform.localPosition += Vector3.up * (_boxHeight * (_heldBoxes.Count - 1));
+                box.transform.localPosition  = Vector3.right + Vector3.up * (_boxHeight * (_heldBoxes.Count - 1));
                 box.transform.localRotation = Quaternion.identity;
 
                 Debug.Log($"Picked up box! Total held boxes: {_heldBoxes.Count}");
+
+                _heldBoxesTop++;
             }
         }
         else
@@ -94,11 +96,12 @@ public class MiniGameUnloadPlayerController : IPlayerController
 
     private void DropBox()
     {
-        /*if (_heldBoxes.Count > 0)
+        Debug.Log("Drop box");
+        if (_heldBoxes.Count > 0 && _heldBoxes.Contains(_heldBoxes[_heldBoxesTop-1]))
         {
             // 스택에서 가장 위에 있는 상자 제거
-            GameObject box = _heldBoxes.Top().gameObject;
-
+            GameObject box = _heldBoxes[_heldBoxesTop-1].gameObject;
+            _heldBoxes.RemoveAt(_heldBoxesTop-1);
             // 상자의 Rigidbody 활성화
             var boxRigidbody = box.GetComponent<Rigidbody>();
             if (boxRigidbody != null)
@@ -106,15 +109,28 @@ public class MiniGameUnloadPlayerController : IPlayerController
                 boxRigidbody.isKinematic = false; // 물리 효과 다시 활성화
             }
 
+            var boxCollision  = box.GetComponent<BoxCollider>();
+            if (boxCollision != null)
+            {
+                boxCollision.isTrigger = false;
+            }
+
+            
             // 상자를 플레이어의 발 아래로 놓기
             box.transform.SetParent(Managers.MiniGame.Root.transform);
-
+            
             Debug.Log($"Dropped box! Remaining boxes: {_heldBoxes.Count}");
+            _heldBoxesTop--;
         }
         else
         {
             Debug.Log("No boxes to drop!");
-        }*/
+        }
     }
-    
+
+    public void ChangeInteraction()
+    {
+        Debug.Log("Change interaction");
+        IsDropBox = !IsDropBox;
+    }
 }
