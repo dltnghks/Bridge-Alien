a140a3ac8d675da7a832d639727a0fa119c63424
LeeSooHwan <dltrnghks@naver.com>
2024-11-26 12:57:14 +0900

    Feat : MiniGameUnload Player Input UI
    - 일단 조이스틱이랑 상호작용 버튼 PlayerInput에 두고, 그 외 UI 변경이나 다른 함수는 상속받아서 처리

--- Assets/03.Scripts/UI/UISubItem/UIPlayerInput.cs
+++ Assets/03.Scripts/UI/UISubItem/UIPlayerInput.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class UIPlayerInput : UISubItem
 {
@@ -15,26 +18,33 @@ public class UIPlayerInput : UISubItem
         InteractionButton,
     }
     
-    private Joystick _joystick;
-
+    protected Joystick _joystick;
+    protected Button _interactionButton;
     public override bool Init()
     {
         if (base.Init() == false)
         {
             return false;
         }
-
         BindObject(typeof(Objects));
         BindButton(typeof(Buttons));
         
         _joystick = GetObject((int)Objects.Joystick).GetOrAddComponent<Joystick>();
-        GetButton((int)Buttons.InteractionButton).gameObject.BindEvent(OnClickInteractionButton);
-        Debug.Log(_joystick.Input);
+        _interactionButton = GetButton((int)Buttons.InteractionButton);
+
+        _interactionButton.gameObject.BindEvent(OnClickInteractionButton);
         
         return true;
     }
 
-    private void OnClickInteractionButton()
+    public void OnJoyStick(InputValue _value){
+        if (Managers.MiniGame.CurrentGame.IsActive)
+        {
+            Managers.MiniGame.CurrentGame.PlayerController.InputJoyStick(_joystick.Direction);
+        }
+    }
+
+    protected  void OnClickInteractionButton()
     {
         Managers.MiniGame.CurrentGame.PlayerController.Interaction();
     }
