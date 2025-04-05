94c6456d92eee280a6fbef3da46d53702003722a
rbase0218 <rbase@naver.com>
2025-04-05 14:54:22 +0900

    Fix : 스폰을 실행할 StartWidth와 EndWidth 지정

--- Assets/03.Scripts/Content/MiniGame/Delivery/Hurdle/GroundManager.cs
+++ Assets/03.Scripts/Content/MiniGame/Delivery/Hurdle/GroundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,33 @@ public class GroundManager : MonoBehaviour
 {
     [SerializeField] private List<GroundPositionPicker> groundPositionPickers;
 
-    private void Awake()
+    [SerializeField] private float startRatio;
+    [SerializeField] private float endRatio;
+    
+    private float _startWidth;
+    private float _endWidth;
+
+    private void Start()
     {
+        if (groundPositionPickers != null)
+        {
+            _startWidth = groundPositionPickers[0].GetPosition(startRatio, startRatio).x;
+            _endWidth = groundPositionPickers[^1].GetPosition(endRatio, endRatio).x;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        _startWidth = groundPositionPickers[0].GetPosition(startRatio, startRatio).x;
+        _endWidth = groundPositionPickers[^1].GetPosition(endRatio, endRatio).x;
         
+        Vector3 startPosition = new Vector3(_startWidth, 1f, transform.position.z);
+        Vector3 endPosition = new Vector3(_endWidth, 1f, transform.position.z);
+        
+        Debug.DrawLine(startPosition, endPosition, Color.black);
+        
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.Label(startPosition, $"Start Point: ({startPosition.x:F2}, {startPosition.z:F2})");
+        UnityEditor.Handles.Label(endPosition, $"End Point : ({endPosition.x:F2}, {endPosition.z:F2})");
     }
 }
