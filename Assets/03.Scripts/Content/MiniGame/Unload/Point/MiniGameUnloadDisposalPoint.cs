d1e7a9b2132a00728e635da6e04292e19c632e79
dltnghks <dltrnghks@naver.com>
2025-07-25 14:37:51 +0900

    Feat: 미니게임 Unload 폐기 지점 연출 추가
    - 박스 폐기 시점에 DOTween Sequence를 이용한 애니메이션 연출을 추가했습니다.
    - 경고 램프 파티클이 재생됩니다.
    - 바닥이 좌우로 열리며 박스가 아래로 떨어집니다.
    - 바닥이 다시 닫히고 사운드 이펙트가 재생됩니다.

--- Assets/03.Scripts/Content/MiniGame/Unload/Point/MiniGameUnloadDisposalPoint.cs
+++ Assets/03.Scripts/Content/MiniGame/Unload/Point/MiniGameUnloadDisposalPoint.cs
@@ -6,14 +6,22 @@ using UnityEngine;
 
 public class MiniGameUnloadDisposalPoint : MiniGameUnloadBasePoint, IBoxPlacePoint
 {
+    [Header("LampParticle")]
+    [SerializeField] private ParticleSystem _lampParticle;
+
+    [Header("Box Drop Area")]
+    [SerializeField] private Transform _dropAreaLeftFloor;
+    [SerializeField] private Transform _dropAreaRightFloor;
+    [SerializeField] private float _moveDistance = 0.3f; // 좌우 이동 거리
+
     // 폐기하는 구역
     [Header("Setting")]
-    private int _maxIndex = 3;
-
+    [SerializeField] private int _maxIndex = 3;
     [SerializeField] private Transform _dropTransform; // Inspector에서 할당
     [SerializeField] private Vector3 _boxDropPosition;
     private float _boxHeight = 0;
     private float _boxHeightOffset = 0.8f;
+    private bool _isDisposing = false;
 
     private MiniGameUnloadBoxList _boxList = new MiniGameUnloadBoxList();
     private Action _triggerAction;
@@ -26,6 +34,7 @@ public class MiniGameUnloadDisposalPoint : MiniGameUnloadBasePoint, IBoxPlacePoi
 
         // 드롭 위치 설정
         _boxDropPosition = _dropTransform != null ? _dropTransform.position : transform.position;
+        _boxDropPosition.y += 0.5f; // 드롭 위치 높이 설정
     }
 
     public void SetDisposalPoint(Action triggerAction)
@@ -75,7 +84,7 @@ public class MiniGameUnloadDisposalPoint : MiniGameUnloadBasePoint, IBoxPlacePoi
 
             box.transform.DOMove(_boxDropPosition + (Vector3.up * _boxHeight * _boxHeightOffset), 1f).OnComplete(() =>
             {
-                if (_boxList.IsFull)
+                if (_boxList.IsFull && !_isDisposing)
                 {
                     DisposeAllBoxes();
                 }
@@ -89,16 +98,54 @@ public class MiniGameUnloadDisposalPoint : MiniGameUnloadBasePoint, IBoxPlacePoi
     // 리스트에 있는 박스 모두 폐기
     public void DisposeAllBoxes()
     {
+        // 처리 중이거나 박스가 없으면 반환
+        if (_isDisposing || _boxList.IsEmpty)
+            return;
+
+        _isDisposing = true;
+        // DisposalBody 에 있는 사이렌 점등 -> DisposalArea 바닥이 열리는 애니메이션
+        _lampParticle.Play();
+
+        Sequence disposeSequence = DOTween.Sequence();
+
+        // DropArea가 좌우로 이동
+        disposeSequence.Append(
+            _dropAreaLeftFloor.DOLocalMoveX(_dropAreaLeftFloor.localPosition.x - _moveDistance, 3f)
+        );
+        disposeSequence.Join(
+            _dropAreaRightFloor.DOLocalMoveX(_dropAreaRightFloor.localPosition.x + _moveDistance, 3f)
+        );
+
+        disposeSequence.AppendInterval(0.1f);
         foreach (var box in _boxList.BoxList)
         {
             if (box != null)
             {
-                box.SetInGameActive(false);
+                disposeSequence.Join(box.transform.DOLocalMoveY(-5, 1f).OnComplete(() => box.SetInGameActive(false))); // 박스가 폐기되는 위치로 이동
             }
         }
-        _boxList.ClearBoxList();
-        _boxHeight = 0; // 높이 초기화
 
-        Managers.Sound.PlaySFX(SoundType.MiniGameUnloadSFX, MiniGameUnloadSoundSFX.Discard.ToString(), gameObject);
+        // DropArea가 원래 위치로 돌아옴
+        disposeSequence.Append(
+            _dropAreaLeftFloor.DOLocalMoveX(_dropAreaLeftFloor.localPosition.x, 1f)
+        );
+        disposeSequence.Join(
+            _dropAreaRightFloor.DOLocalMoveX(_dropAreaRightFloor.localPosition.x, 1f)
+        );
+
+        disposeSequence.OnComplete(() =>
+        {
+            // 이펙트 중지 및 초기화
+            _lampParticle.Pause();
+            _lampParticle.Clear();
+
+            _boxList.ClearBoxList();
+            _boxHeight = 0; // 높이 초기화
+
+            Managers.Sound.PlaySFX(SoundType.MiniGameUnloadSFX, MiniGameUnloadSoundSFX.Discard.ToString(), gameObject);
+            _isDisposing = false;
+        });
+        
+        disposeSequence.Play();
     }
 }
