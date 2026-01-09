f145effadf50c4476cde82e3f63d7495b4dcd81b
dltrnghks0507 <dltrnghks0507@gmail.com>
2026-01-09 22:51:08 +0900

    fix : 1스테이지 취급주의 예외처리 및 테스트 씬 수정

--- Assets/03.Scripts/Content/Scene/StageEditorScene.cs
+++ Assets/03.Scripts/Content/Scene/StageEditorScene.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class StageEditorScene : BaseScene
 {
@@ -11,8 +14,34 @@ public class StageEditorScene : BaseScene
 
         SceneType = Define.Scene.StageEditor;
 
+#if UNITY_EDITOR
+        var stageType = GetStageTypeFromEditorPrefs();
+        if (stageType.HasValue)
+        {
+            Managers.Stage.SetCurrentStage(stageType.Value);
+        }
+#endif
+
         Managers.MiniGame.LoadStageEditor();
         return true;
     }
+
+#if UNITY_EDITOR
+    private Define.ChapterType? GetStageTypeFromEditorPrefs()
+    {
+        string selectedStageName = EditorPrefs.GetString("StageEditor_SelectedStage", "");
+        if (string.IsNullOrEmpty(selectedStageName))
+        {
+            return null;
+        }
+
+        if (System.Enum.TryParse<Define.ChapterType>(selectedStageName, out var stageType))
+        {
+            return stageType;
+        }
+
+        return null;
+    }
+#endif
 }
 
