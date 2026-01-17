06ea54011c66025fba3e70081e57c63ac1f31134
dltrnghks0507 <dltrnghks0507@gmail.com>
2026-01-17 14:31:11 +0900

    chore : 데이터 적용

--- Assets/03.Scripts/Data/StageData.cs
+++ Assets/03.Scripts/Data/StageData.cs
@@ -12,6 +12,7 @@ public class StageData
     public string StageDescription;         // 스테이지 설명
     public bool IsLocked;                   // 초기 잠금, 1스테이지는 false로 설정
     public int RequiredStars;               // 스테이지 해금에 필요한 별 개수
+    public int MinimumWage;                 // 최소 임금
     public int ClearReward;                 // 클리어 보상
     public int[] ClearScoreList;            // 클리어 스코어 - 크기 = 최대 별 개수
     public Define.EventDataID EventID;
--- Assets/03.Scripts/Editor/StageEditor.cs
+++ Assets/03.Scripts/Editor/StageEditor.cs
@@ -186,6 +186,7 @@ public class StageEditor : EditorWindow
         data.IsLocked = EditorGUILayout.Toggle("Is Locked", data.IsLocked);
         data.RequiredStars = EditorGUILayout.IntField("Required Stars", data.RequiredStars);
         data.ClearReward = EditorGUILayout.IntField("Clear Reward", data.ClearReward);
+        data.MinimumWage = EditorGUILayout.IntField("Minimum Wage", data.MinimumWage);
         data.EventID = (Define.EventDataID)EditorGUILayout.EnumPopup("Event ID", data.EventID);
         data.ClearEventID = (Define.EventDataID)EditorGUILayout.EnumPopup("Clear Event ID", data.ClearEventID);
 
